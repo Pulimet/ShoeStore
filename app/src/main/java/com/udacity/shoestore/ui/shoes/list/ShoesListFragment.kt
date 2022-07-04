790c3f0c7d8cfd83d93286925891bb998194e2ed
Pulimet <korolev2009@gmail.com>
2022-07-04 09:34:00 +0300

    Support Logout

--- app/src/main/java/com/udacity/shoestore/ui/shoes/list/ShoesListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoes/list/ShoesListFragment.kt
@@ -1,17 +1,43 @@
 package com.udacity.shoestore.ui.shoes.list
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoesListBinding
 import com.udacity.shoestore.ui.binding.FragmentBinding
 
-class ShoesListFragment : Fragment(R.layout.fragment_shoes_list) {
+class ShoesListFragment : Fragment(R.layout.fragment_shoes_list), MenuProvider {
 
     private val binding by FragmentBinding(FragmentShoesListBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupMenu()
+    }
+
+    private fun setupMenu() {
+        requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    // MenuProvider
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_shoes_list, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.action_logout -> {
+                findNavController().navigate(ShoesListFragmentDirections.actionShoesListFragmentToLoginFragment())
+                true
+            }
+            else -> false
+        }
     }
 }
