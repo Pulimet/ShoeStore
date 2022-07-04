478ceebe85b8277fe0278feb96e48285a14da8bc
Pulimet <korolev2009@gmail.com>
2022-07-04 10:37:55 +0300

    ShoeDetailsFragment.kt - Support adding shoes

--- app/src/main/java/com/udacity/shoestore/ui/shoes/ShoesViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoes/ShoesViewModel.kt
@@ -7,6 +7,14 @@ import com.udacity.shoestore.models.Shoe
 
 class ShoesViewModel : ViewModel() {
     private val _shoesList = MutableLiveData<List<Shoe>>()
+
     val shoesList: LiveData<List<Shoe>>
         get() = _shoesList
+
+    fun addShoe(shoeName: String, company: String, size: String, description: String) {
+        val shoe = Shoe(shoeName, size.toDouble(), company, description, emptyList())
+        val list = _shoesList.value?.toMutableList() ?: mutableListOf()
+        list.add(shoe)
+        _shoesList.value = list
+    }
 }
--- app/src/main/java/com/udacity/shoestore/ui/shoes/details/ShoeDetailsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoes/details/ShoeDetailsFragment.kt
@@ -3,15 +3,41 @@ package com.udacity.shoestore.ui.shoes.details
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailsBinding
 import com.udacity.shoestore.ui.binding.FragmentBinding
+import com.udacity.shoestore.ui.shoes.ShoesViewModel
 
-class ShoeDetailsFragment : Fragment(R.layout.fragment_shoe_details) {
+class ShoeDetailsFragment : Fragment(R.layout.fragment_shoe_details), View.OnClickListener {
 
     private val binding by FragmentBinding(FragmentShoeDetailsBinding::bind)
+    private val viewModel: ShoesViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.btnCancel.setOnClickListener(this)
+        binding.btnSave.setOnClickListener(this)
+    }
+
+    // View.OnClickListener
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnCancel -> findNavController().navigateUp()
+            R.id.btnSave -> {
+                viewModel.addShoe(
+                    binding.tilShowName.editText?.text?.toString() ?: "",
+                    binding.tilCompany.editText?.text?.toString() ?: "",
+                    binding.tilShowSize.editText?.text?.toString() ?: "",
+                    binding.tilDescription.editText?.text?.toString() ?: "",
+                )
+                findNavController().navigateUp()
+            }
+        }
     }
 }
