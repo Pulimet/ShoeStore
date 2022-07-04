b2d30f5a42660be52b1f57531076698ee9a3df5c
Pulimet <korolev2009@gmail.com>
2022-07-04 08:55:01 +0300

    LoginFragment.kt - On Password action done navigate to Welcome Fragment

--- app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
@@ -1,25 +1,29 @@
 package com.udacity.shoestore.ui.login
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 import com.udacity.shoestore.ui.binding.FragmentBinding
 
-class LoginFragment : Fragment(R.layout.fragment_login), View.OnClickListener {
+class LoginFragment : Fragment(R.layout.fragment_login), View.OnClickListener, TextView.OnEditorActionListener {
 
     private val binding by FragmentBinding(FragmentLoginBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setClickListener()
+        setListeners()
     }
 
-    private fun setClickListener() {
+    private fun setListeners() {
         binding.btnSignIn.setOnClickListener(this)
         binding.btnSignUp.setOnClickListener(this)
+        binding.tilPassword.editText?.setOnEditorActionListener(this)
     }
 
     // View.OnClickListener
@@ -29,4 +33,13 @@ class LoginFragment : Fragment(R.layout.fragment_login), View.OnClickListener {
             R.id.btnSignUp -> findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
         }
     }
+
+    // TextView.OnEditorActionListener
+    override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?) =
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+            true
+        } else {
+            false
+        }
 }
