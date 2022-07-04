b817f7fd938be7dba1ab6031113e977987698e2a
Pulimet <korolev2009@gmail.com>
2022-07-04 10:47:15 +0300

    ShoeDetailsFragment.kt - UI + save values on rotation

--- app/src/main/java/com/udacity/shoestore/ui/shoes/details/ShoeDetailsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoes/details/ShoeDetailsFragment.kt
@@ -1,7 +1,10 @@
 package com.udacity.shoestore.ui.shoes.details
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -10,7 +13,8 @@ import com.udacity.shoestore.databinding.FragmentShoeDetailsBinding
 import com.udacity.shoestore.ui.binding.FragmentBinding
 import com.udacity.shoestore.ui.shoes.ShoesViewModel
 
-class ShoeDetailsFragment : Fragment(R.layout.fragment_shoe_details), View.OnClickListener {
+class ShoeDetailsFragment : Fragment(R.layout.fragment_shoe_details), View.OnClickListener,
+    TextView.OnEditorActionListener {
 
     private val binding by FragmentBinding(FragmentShoeDetailsBinding::bind)
     private val viewModel: ShoesViewModel by activityViewModels()
@@ -23,21 +27,33 @@ class ShoeDetailsFragment : Fragment(R.layout.fragment_shoe_details), View.OnCli
     private fun setListeners() {
         binding.btnCancel.setOnClickListener(this)
         binding.btnSave.setOnClickListener(this)
+        binding.tilDescription.editText?.setOnEditorActionListener(this)
+    }
+
+    private fun saveShoeAndGoBack() {
+        viewModel.addShoe(
+            binding.tilShoeName.editText?.text?.toString() ?: "",
+            binding.tilCompany.editText?.text?.toString() ?: "",
+            binding.tilShowSize.editText?.text?.toString() ?: "",
+            binding.tilDescription.editText?.text?.toString() ?: "",
+        )
+        findNavController().navigateUp()
     }
 
     // View.OnClickListener
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btnCancel -> findNavController().navigateUp()
-            R.id.btnSave -> {
-                viewModel.addShoe(
-                    binding.tilShowName.editText?.text?.toString() ?: "",
-                    binding.tilCompany.editText?.text?.toString() ?: "",
-                    binding.tilShowSize.editText?.text?.toString() ?: "",
-                    binding.tilDescription.editText?.text?.toString() ?: "",
-                )
-                findNavController().navigateUp()
-            }
+            R.id.btnSave -> saveShoeAndGoBack()
         }
     }
+
+    // TextView.OnEditorActionListener
+    override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?) =
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            saveShoeAndGoBack()
+            true
+        } else {
+            false
+        }
 }
